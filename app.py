bd7d4d0e275dbe5578521c1439cba528e3e8b17e
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2024-02-06 12:27:25 +0200

    add starter

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Привіт, світ! Це мій перший бекенд на Python з Flask і він працює!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
